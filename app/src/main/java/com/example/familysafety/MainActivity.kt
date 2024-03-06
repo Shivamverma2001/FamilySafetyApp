a60b580a6301fcaf4c48c6d78c4b6cb06a4cc505
Shivam verma <shivamverma@Shivams-MacBook-Air-3.local>
2024-03-06 16:04:23 +0530

    Bottom Navigation Fragment

--- app/src/main/java/com/example/familysafety/MainActivity.kt
+++ app/src/main/java/com/example/familysafety/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.familysafety
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigation:BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation=findViewById(R.id.bottom_bar)
+        bottomNavigation.setOnItemSelectedListener {menuItem->
+            if(menuItem.itemId==R.id.nav_guard){
+                    inflateFragment(GuardFragment.newInstance())
+            }else if(menuItem.itemId==R.id.nav_home){
+                inflateFragment(HomeFragment.newInstance())
+            }
+            else if(menuItem.itemId==R.id.nav_dashboard){
+                inflateFragment(DashboardFragment.newInstance())
+            }
+            else if(menuItem.itemId==R.id.nav_profile){
+                inflateFragment(UserFragment.newInstance())
+            }
+            true
+        }
+    }
+
+    private fun inflateFragment(newInstance:Fragment) {
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,newInstance)
+        transaction.commit()
     }
 }
